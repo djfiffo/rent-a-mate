e4fa5c4dee5e71b21f12ee3f93ea9662805b7144
Jirath Mahathanaseth <jirath@MacBook-Pro-khxng-Jirath.local>
2026-09-07 19:28:54 +0700

    feat(auth):implement refreshtoken logic and refresh and logout service

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
@@ -15,5 +16,15 @@ export class AuthController {
 	@Post('login')
 	login(@Body() dto: LoginDto) {
 		return this.authService.login(dto);
+	}
+
+	@Post('refresh')
+	refresh(@Body() dto: RefreshTokenDto) {
+		return this.authService.refresh(dto.refreshToken);
+	}
+
+	@Post('logout')
+	logout(@Body() dto: RefreshTokenDto) {
+		return this.authService.logout(dto.refreshToken);
 	}
 }
