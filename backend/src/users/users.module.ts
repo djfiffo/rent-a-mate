837549f20f5fa2a24d67a17a08a4766988b643de
Jirath Mahathanaseth <jirath@MacBook-Pro-khxng-Jirath.local>
2026-09-12 17:49:45 +0700

    fix:import auth module in users module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { db } from '../prisma/db.js';
+import { AuthModule } from '../auth/auth.module.js';
 import { BcryptPasswordService } from './password.service.js';
 import { DATABASE_TOKEN, PASSWORD_SERVICE } from './users.tokens.js';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 
 @Module({
+  imports: [AuthModule],
+  
   controllers: [UsersController],
   providers: [
     UsersService,
